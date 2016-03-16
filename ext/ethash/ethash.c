67629694007b5268f80bb66c93e916e5fe28c89e
Jan Xie <jan.h.xie@gmail.com>
2016-03-16 21:42:35 +0800

    Ethash.hashimoto_light

--- ext/ethash/ethash.c
+++ ext/ethash/ethash.c
@@ -7,18 +7,52 @@
 static VALUE rb_mEthash;
 
 static VALUE
-ethash_mkcache_bytes(VALUE self, VALUE n) {
-    unsigned long block_number = NUM2LONG(n);
+ethash_mkcache_bytes(VALUE self, VALUE blknum) {
+  unsigned long block_number = NUM2ULONG(blknum);
 
-    ethash_light_t L = ethash_light_new(block_number);
-    VALUE val = rb_str_new(L->cache, L->cache_size);
-    free(L->cache);
+  ethash_light_t L = ethash_light_new(block_number);
+  VALUE val = rb_str_new(L->cache, L->cache_size);
+  free(L->cache);
 
-    return val;
+  return val;
+}
+
+static VALUE
+ethash_hashimoto_light(VALUE self, VALUE blknum, VALUE cache, VALUE header, VALUE _nonce) {
+  char *cache_bytes = RSTRING_PTR(cache);
+  unsigned long cache_size = RSTRING_LEN(cache);
+
+  char *header_bytes = RSTRING_PTR(header);
+  unsigned long header_size = RSTRING_LEN(header);
+
+  unsigned long block_number = NUM2ULONG(blknum);
+  unsigned long nonce = NUM2ULL(_nonce);
+
+  struct ethash_light *s;
+  s = calloc(sizeof(*s), 1);
+  s->cache = cache_bytes;
+  s->cache_size = cache_size;
+  s->block_number = block_number;
+
+  struct ethash_h256 *h;
+  h = calloc(sizeof(*h), 1);
+  for(unsigned int i=0; i<32; i++) h->b[i] = header_bytes[i];
+
+  struct ethash_return_value out = ethash_light_compute(s, *h, nonce);
+
+  VALUE hash = rb_hash_new();
+  VALUE key_mixhash = ID2SYM(rb_intern("mixhash"));
+  VALUE key_result = ID2SYM(rb_intern("result"));
+  rb_hash_aset(hash, key_mixhash, rb_str_new(&out.mix_hash, 32));
+  rb_hash_aset(hash, key_result, rb_str_new(&out.result, 32));
+
+  return hash;
 }
 
 void
 Init_ethash() {
+
   rb_mEthash = rb_define_module("Ethash");
   rb_define_singleton_method(rb_mEthash, "mkcache_bytes", ethash_mkcache_bytes, 1);
+  rb_define_singleton_method(rb_mEthash, "hashimoto_light", ethash_hashimoto_light, 4);
 }
--- test/ethash_test.rb
+++ test/ethash_test.rb
@@ -8,7 +8,14 @@ require 'ethash'
 class EthashTest < Minitest::Test
 
   def test_mkcache_bytes
-    assert_equal 16776896, Ethash.mkcache_bytes(0)
+    assert_equal 16776896, Ethash.mkcache_bytes(0).size
+  end
+
+  def test_hashimoto_light
+    cache = Ethash.mkcache_bytes(0)
+    header = "\x01"*32
+    hash = {mixhash: "\xD5\x7F\xB3|OaU\x06\x12\xC0;\x9D\xFB\xC5\xD5;k\x11\xB9\x84\xEFU\xAC\xD7\xDDh\xEC\xF5VF\xA1\xF3", result: "E~\xE8\xA0i\xD4\x18\x95iU\xF9\xA5k*\x02\x1E\xAE~\xBD\x8EoM\x10#\xB67\xFA\xAD)4\x1E\xDD"}
+    assert_equal hash, Ethash.hashimoto_light(0, cache, header, 0)
   end
 
 end
